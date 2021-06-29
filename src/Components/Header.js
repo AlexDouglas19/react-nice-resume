3bf8cfc0fcd62614e8d6c640e94e7dfde5fe14b0
AlexDouglas19 <86575653+AlexDouglas19@users.noreply.github.com>
2021-06-29 15:49:07 +1200

    initial changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -38,19 +38,7 @@ class Header extends Component {
 
             <li>
               <a className="smoothscroll" href="#resume">
-                Resume
-              </a>
-            </li>
-
-            <li>
-              <a className="smoothscroll" href="#portfolio">
-                Works
-              </a>
-            </li>
-
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
+                Services
               </a>
             </li>
           </ul>
@@ -63,18 +51,7 @@ class Header extends Component {
             </Fade>
             <Fade bottom duration={1200}>
               <h3>{description}.</h3>
-            </Fade>
-            <hr />
-            <Fade bottom duration={2000}>
-              <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
-                </a>
-                <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
-                </a>
-              </ul>
-            </Fade>
+            </Fade>            
           </div>
         </div>
 
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
+import { testModeAPI } from "react-ga";
 import Slide from "react-reveal";
 
 class Resume extends Component {
-  getRandomColor() {
-    let letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
+  
   render() {
     if (!this.props.data) return null;
 
@@ -28,6 +21,7 @@ class Resume extends Component {
       );
     });
 
+
     const work = this.props.data.work.map(function (work) {
       return (
         <div key={work.company}>
@@ -41,26 +35,32 @@ class Resume extends Component {
       );
     });
 
-    const skills = this.props.data.skills.map((skills) => {
-      const backgroundColor = this.getRandomColor();
-      const className = "bar-expand " + skills.name.toLowerCase();
-      const width = skills.level;
-
-      return (
-        <li key={skills.name}>
-          <span style={{ width, backgroundColor }} className={className}></span>
-          <em>{skills.name}</em>
-        </li>
-      );
-    });
-
     return (
       <section id="resume">
+
+        <Slide left duration={1300}>
+          <div className="row work">
+            <div className="three columns header-col">
+              <h1>
+                <span>Services</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <p className="info"><em className="date">Service 1</em></p>
+              <p className="info"><em className="date">Service 2</em></p>
+              <p className="info"><em className="date">Service 3</em></p>
+              <p className="info"><em className="date">Service 4</em></p>
+              <p className="info"><em className="date">Service 5</em></p>
+            </div>
+          </div>
+        </Slide>
+
         <Slide left duration={1300}>
           <div className="row education">
             <div className="three columns header-col">
               <h1>
-                <span>Education</span>
+                <span>Experience</span>
               </h1>
             </div>
 
@@ -72,35 +72,6 @@ class Resume extends Component {
           </div>
         </Slide>
 
-        <Slide left duration={1300}>
-          <div className="row work">
-            <div className="three columns header-col">
-              <h1>
-                <span>Work</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">{work}</div>
-          </div>
-        </Slide>
-
-        <Slide left duration={1300}>
-          <div className="row skill">
-            <div className="three columns header-col">
-              <h1>
-                <span>Skills</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">
-              <p>{skillmessage}</p>
-
-              <div className="bars">
-                <ul className="skills">{skills}</ul>
-              </div>
-            </div>
-          </div>
-        </Slide>
       </section>
     );
   }
